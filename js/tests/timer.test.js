23226cd0bc950730ea9e4beb1ef06e010fd9e8b4
VitaliyBondarenko1982 <vitaliybondarenko@ukr.net>
2018-06-26 22:04:27 +0300

    solved errors

--- js/tests/timer.test.js
+++ js/tests/timer.test.js
@@ -2,10 +2,6 @@ import {assert} from 'chai';
 import {setGameTimer} from '../timer';
 
 describe(`Check Game Timer`, () => {
-  it(`Should return, that time is out`, () => {
-    assert.equal(setGameTimer(0).isTimeout, true);
-  });
-
   it(`should return time reduced by one`, () => {
     assert.equal(setGameTimer(100).tick(), 99);
   });
